8720503565c0a01e0b5d14e31243920b1aecefa8
abhaytagad <abhaytagad2812@gmail.com>
2025-08-29 14:34:26 +0530

    Added Docker setup for producer & consumer

--- result-metadata-service/src/main/java/com/example/result_metadata_service/controller/ResultMetadataController.java
+++ result-metadata-service/src/main/java/com/example/result_metadata_service/controller/ResultMetadataController.java
@@ -3,15 +3,7 @@ package com.example.result_metadata_service.controller;
 import com.example.result_metadata_service.model.ResultMetadata;
 import com.example.result_metadata_service.service.ResultMetadataService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.*;
 @CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
